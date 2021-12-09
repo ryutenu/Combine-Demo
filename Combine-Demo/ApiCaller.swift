169cec7be822f8100e9a3e8d45915e78fd4b0c9e
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-09 12:50:37 +0900

    [Add] ApiCaller

---
+++ Combine-Demo/ApiCaller.swift
@@ -0,0 +1,21 @@
+//
+//  ApiCaller.swift
+//  Combine-Demo
+//
+//  Created by Alan Liu on 2021/12/09.
+//
+
+import UIKit
+import Combine
+
+class ApiCaller {
+    static let shared = ApiCaller()
+    
+    func fetchCompanyList() -> Future<[String], Error> {
+        return Future { promise in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                promise(.success(["Tesla", "Alphabet", "Amazon", "Meta", "Apple", "Microsoft"]))
+            }
+        }
+    }
+}
--- Combine-Demo/ViewController.swift
+++ Combine-Demo/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
     
@@ -15,26 +16,42 @@ class ViewController: UIViewController {
         return tableView
     }()
     
+    private var companyList = [String]()
+    
+    var observer: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(tableView)
         tableView.dataSource = self
         tableView.frame = view.bounds
+        
+        observer = ApiCaller.shared.fetchCompanyList()
+            .receive(on: DispatchQueue.main, options: nil)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished: print("finished")
+                case .failure(let error): print(error)
+                }
+        }, receiveValue: { [weak self] value in
+            self?.companyList = value
+            self?.tableView.reloadData()
+        })
     }
 }
 
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return companyList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomCell else {
             fatalError()
         }
-        
+        cell.textLabel?.text = companyList[indexPath.row]
         return cell
     }
 }
