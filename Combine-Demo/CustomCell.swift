2661f5a5e5ee668de0e58435baaec69dce68b6c2
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-09 12:13:30 +0900

    [Add] CustomCell

---
+++ Combine-Demo/CustomCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCell.swift
+//  Combine-Demo
+//
+//  Created by Alan Liu on 2021/12/09.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+}
--- Combine-Demo/ViewController.swift
+++ Combine-Demo/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(CustomCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomCell else {
+            fatalError()
+        }
+        
+        return cell
+    }
+}
